1a9c9ab78e5d8d47644e046621a01829c76ebaa3
NickCyrus <nicklemusduque@gmail.com>
2017-08-28 17:55:52 -0500

    update

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+ </head>
+ <style>
+   body , html{
+       width:100%;
+       height:100%;
+       overflow: hidden;
+       border:0px;
+       padding:0px;
+       margin:0px;
+   }
+
+   iframe{
+       width:100%;
+       height:100%;
+       border:none;
+   }
+ </style>
+ <body>
+   <iframe src="http://192.168.1.14/webview/?rand=<?=rand()?>">
+
+   </iframe>
+ </body>
+</html>
